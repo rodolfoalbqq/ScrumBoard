f3fc3e0456539326ff5c6625e33f4b40fa1fd4af
rodolfoalbqq <rodolfo.albqq@gmail.com>
2025-03-20 20:19:56 -0300

    Initial commit. Feat(Rodolfo)

---
+++ ScrumBoard/src/main/java/ScrumBoard/ScrumBoardApplication.java
@@ -0,0 +1,13 @@
+package ScrumBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumBoardApplication.class, args);
+	}
+
+}
---
+++ ScrumBoard/src/test/java/ScrumBoard/ScrumBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ScrumBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
